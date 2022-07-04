0acd43e8564c41fa1e443b963268f4b76e542580
Steven McCawley <70384340+StevenMcCawley@users.noreply.github.com>
2022-07-04 15:47:31 -0500

    initial commit

---
+++ index.jsx
@@ -0,0 +1,73 @@
+const ATMDeposit = ({ onChange }) => {
+  return (
+    <label>
+      Deposit
+      <input
+        className="bg-light mx-2"
+        type="number"
+        onChange={onChange}
+      ></input>
+      <input type="submit"></input>
+    </label>
+  );
+};
+
+const ATMWithdraw = ({ onChange }) => {
+  return (
+    <label>
+      Withdraw
+      <input
+        className="bg-light mx-2"
+        type="number"
+        onChange={onChange}
+      ></input>
+      <input type="submit"></input>
+    </label>
+  );
+};
+
+const Account = () => {
+  const [accountState, setAccountState] = React.useState(0);
+  var deposit = 0;
+  var withdraw = 0;
+  const handleDeposit = (event) => {
+    let val = event.target.value;
+    val = Number(val);
+    if (val < 0) return;
+    console.log(`handleDeposit: ${val}`);
+    deposit = val;
+  };
+  const handleWithdraw = (event) => {
+    let val = event.target.value;
+    val = Number(val);
+    if (val < 0) return;
+    console.log(`handleWithdraw: ${val}`);
+    withdraw = val;
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let newTotal = accountState + deposit - withdraw;
+    if (newTotal < 0) {
+      alert("cannot have a negative account balance");
+      return;
+    }
+    deposit = withdraw = 0;
+    event.target.value = 0;
+    // alert(`Account total = ${newTotal}`);
+    setAccountState(newTotal);
+  };
+
+  return (
+    <>
+      <h2>Account Balance: ${accountState}</h2>
+      <form onSubmit={handleSubmit}>
+        <ATMDeposit onChange={handleDeposit}>Deposit</ATMDeposit>
+      </form>
+      <form onSubmit={handleSubmit}>
+        <ATMWithdraw onChange={handleWithdraw}>Deposit</ATMWithdraw>
+      </form>
+    </>
+  );
+};
+
+ReactDOM.render(<Account />, document.querySelector(`#root`));
